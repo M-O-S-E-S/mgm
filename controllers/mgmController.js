5f312c56daaa6f7d71845d1bc075fcb5c0e41a16
Michael Heilmann <mheilman@ist.ucf.edu>
2014-03-26 09:38:35 -0400

    fixed login vs resume, fixed routing on sessions

--- controllers/mgmController.js
+++ controllers/mgmController.js
@@ -21,13 +21,13 @@ angular.module('mgmApp')
             $http.post("/server/auth/login",{ 'username':this.userName, 'password': this.password }).success(function(data, status, headers, config){
                 if(data.Success){
                     console.log("login successfull");
-                    $scope.auth.activeUser = new User(data.username, data.uuid, data.email, data.accessLevel, []);
+                    $scope.auth.activeUser = { name:data.username, uuid:data.uuid, email:data.email, accessLevel: data.accessLevel, identities: [{Enabled: true}]};
                     $scope.auth.loggedIn = true;
                     $scope.auth.userName = "";
                     $scope.auth.password = "";
                     $scope.updater = $interval(function(){ $rootScope.$broadcast('mgmUpdate','trigger'); }, 10*1000);
                     $rootScope.$broadcast('mgmUpdate','trigger');
-                    $scope.location.goto('/account');
+                    $location.path('/account');
                 } else {
                     console.log(data.Message);
                     alertify.error(data.Message);
@@ -49,7 +49,7 @@ angular.module('mgmApp')
                     $rootScope.$broadcast('mgmUpdate','trigger'); 
                 } else {
                     console.log("session resume failed");
-                    $scope.location.goto('/');
+                    $location.path('/');
                 };
             });
         },

e4dd1fb1321ba8d973bed0028385b028c3bea05a
Michael Heilmann <mheilman@ist.ucf.edu>
2017-02-13 11:50:03 -0500

    Added in some functionality to open the door for region console access in the future

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,5 @@
 export { Credential } from './Credential';
 export { UUIDString } from './UUID';
 export { RegionLogs } from './regionLogs';
-export { ConsoleCommand } from './Console';
 export { RegionINI } from './Region';
 export { EmailMgr } from './Email';
--- src/routes/ConsoleHandler.ts
+++ src/routes/ConsoleHandler.ts
@@ -1,73 +1,62 @@
 
 import * as express from 'express';
-import { UUIDString,RegionLogs, ConsoleCommand } from '../lib';
-import { PersistanceLayer, RegionInstance, HostInstance } from '../database';
-import { Config } from '../Config'; 
+import { UUIDString } from '../lib';
+import { PersistanceLayer, RegionInstance, HostInstance, UserInstance } from '../database';
+import { Config } from '../Config';
+
+import * as jwt from 'jsonwebtoken';
+import * as dateFormat from 'dateformat';
+/**
+ * This function generates a Halcyon-style JWT.  JHalcyon is a bit particular, but this works.
+ */
+export function GetConsoleToken(u: UserInstance, cert: Buffer): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    jwt.sign(
+      {
+        Username: u.username + ' ' + u.lastname,
+        Scope: 'remote-console',
+        Exp: dateFormat(new Date().setDate(new Date().getDate() + 2), 'mm/dd/yyyy h:MM:ss'),
+        UserId: u.UUID,
+        BirthDate: new Date(u.created),
+        PartnerId: u.partner
+      },
+      cert,
+      {
+        algorithm: 'RS256',
+        noTimestamp: true
+      },
+      (err: Error, token: string) => {
+        if (err) return reject(err);
+        resolve(token);
+      }
+    );
+  })
+}
 
 export function ConsoleHandler(db: PersistanceLayer, config: Config, isUser: any): express.Router {
   let router = express.Router();
 
-  let logger = new RegionLogs(config.mgm.log_dir);
-
   router.post('/open/:uuid', isUser, (req, res) => {
     let regionID = new UUIDString(req.params.uuid);
+    let region: RegionInstance;
+    if (req.user.godLevel < 250) {
+      res.send(JSON.stringify({ Success: false, Message: 'Console is restricted to admin' }));
+      return;
+    }
     db.Regions.getByUUID(regionID.toString()).then((r: RegionInstance) => {
       if (!r.isRunning) {
         throw new Error('Region must be running to open a console');
       }
-      res.cookie('console', 0);
-      res.send(JSON.stringify({ Success: true, Prompt: r.name + ' >' }));
-    }).catch((err: Error) => {
-      res.send(JSON.stringify({ Success: false, Message: err.message }));
-    });
-  });
-
-  router.post('/read/:uuid', isUser, (req, res) => {
-    if (!req.cookies['console']) {
-      res.send(JSON.stringify({ Success: false, Message: 'No session found' }));
-      return;
-    }
-    let regionID = new UUIDString(req.params.uuid);
-    db.Regions.getByUUID(regionID.toString()).then((r: RegionInstance) => {
-      if (!r.isRunning) {
-        throw new Error('Region is no longer running');
-      }
-      return logger.read(regionID, parseInt(req.cookies['console']));
-    }).then((lines: string[]) => {
-      res.cookie('console', parseInt(req.cookies['console']) + lines.length);
-      res.send(JSON.stringify({ Success: true, Lines: lines }));
-    }).catch((err: Error) => {
-      res.send(JSON.stringify({ Success: false, Message: err.message }));
-    });
-  });
-
-  router.post('/close/:uuid', isUser, (req, res) => {
-    if (!req.cookies['console']) {
-      res.send(JSON.stringify({ Success: false, Message: 'No session found' }));
-      return;
-    }
-    res.clearCookie('console');
-    res.send(JSON.stringify({ Success: true }));
-  });
-
-  router.post('/write/:uuid', isUser, (req, res) => {
-    if (!req.cookies['console']) {
-      res.send(JSON.stringify({ Success: false, Message: 'No session found' }));
-      return;
-    }
-    let regionID = new UUIDString(req.params.uuid);
-    let command: string = req.body.command;
-    let region: RegionInstance;
-    db.Regions.getByUUID(regionID.toString()).then((r: RegionInstance) => {
-      if (!r.isRunning) {
-        throw new Error('Region is no longer running');
-      }
       region = r;
-      return db.Hosts.getByAddress(r.slaveAddress);
-    }).then((h: HostInstance) => {
-      return ConsoleCommand(region, h, command);
-    }).then(() => {
-      res.send(JSON.stringify({ Success: true }));
+      return db.Users.getByID(req.user.uuid);
+    }).then((u: UserInstance) => {
+      return GetConsoleToken(u, config.mgm.certificate);
+    }).then((t: string) => {
+      res.send(JSON.stringify({
+        Success: true,
+        Token: t,
+        URL: 'http://' + region.externalAddress + ':' + region.httpPort
+      }));
     }).catch((err: Error) => {
       res.send(JSON.stringify({ Success: false, Message: err.message }));
     });

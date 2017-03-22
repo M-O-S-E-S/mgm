6d57ee106b9150c78837245543248b1dfa32a4b4
Michael Heilmann <mheilman@ist.ucf.edu>
2017-03-22 13:42:15 -0400

    Split scripts into their own compile script to simplify.

---
+++ script/build-scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p build/scripts
+node_modules/.bin/webpack --config ./script/create-user.config.js
+node_modules/.bin/webpack --config ./script/migrate-db.config.js
+node_modules/.bin/webpack --config ./script/template-existing-user.config.js
